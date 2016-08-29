2eeb69a70b0f788cd7d23d6933d6d00536e4bf8f
Olalekan Bisiriyu <olalekanbisiriyu@yahoo.com>
2016-08-29 05:56:42 -0500

    Initial Commit

---
+++ TheWalkingScrollView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TheWalkingScrollView
+//
+//  Created by olalekan bisiriyu on 2016-08-29.
+//  Copyright © 2016 olalekan bisiriyu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
